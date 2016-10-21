97e254c864cf1a288ce26525247c5b0cb86af69e
Othman Alothman <othman.f.alothman@gmail.com>
2016-10-20 22:20:39 -0400

    Submit mypic app

---
+++ Othman_Alothman_Pic_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Othman_Alothman_Pic_App
+//
+//  Created by Othman AlOthman on 10/20/16.
+//  Copyright © 2016 Othman Alothman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
